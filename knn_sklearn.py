d385c7aadf53d012b9b51a7d9442633d8d2973ac
nasrine-b <nasrine-bahria@outlook.fr>
2021-01-11 09:07:40 +0100

    dev of knn cross-validation

--- knn_sklearn.py
+++ knn_sklearn.py
@@ -4,6 +4,9 @@ import numpy as np
 from sklearn import neighbors, datasets, preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+from matplotlib.axes import Axes
+
+import matplotlib.pyplot as plt
 filename = sys.argv[1]
 
 with h5py.File(filename, "r") as hdf:
@@ -20,16 +23,11 @@ with h5py.File(filename, "r") as hdf:
     # Get the target
     data = hdf.get('User')
     user = np.array(data)
-target = target[0]
-"""
-print(predictor.shape) #Les capteurs
-print(target.shape) #classe du signe
-print(user.shape) #L'utilisateur
-print(predictor[10,1200])
-print(type(predictor))
 
-"""
-features=predictor.T#list(zip(predictor[1],predictor[2],predictor[]))
+target = target[0]
+#   Let's combine all features
+features=predictor.T
+#   Split the data into train and test (80:20)
 X_train, X_test, y_train, y_test = train_test_split(features,target, test_size=0.2)
 
 knn = neighbors.KNeighborsClassifier(n_neighbors=1)
@@ -37,7 +35,28 @@ knn.fit(X_train, y_train)
 y_pred = knn.predict(X_test)
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
 
+#   Problem : we don't have the right value of K
+#   Then let's split the data into train, validation and test (80:15:15)
+X_train, X_test, y_train, y_test = train_test_split(features,target, test_size=0.2)
+X_test, X_val, y_test, y_val = train_test_split(X_test,y_test, test_size=0.15)
 
-#predicted = knn.predict()
-# What kind of iris has 3cm x 5cm sepal and 4cm x 2cm petal?
-#print(iris.target_names[knn.predict([[3, 5, 4, 2]])])
+accuracy_List = []
+list_K = range(1,10)
+for i in list_K:
+    knn = neighbors.KNeighborsClassifier(n_neighbors=i)
+    knn.fit(X_train, y_train)
+    y_pred_val = knn.predict(X_val)
+    accuracy_List.append(metrics.accuracy_score(y_val, y_pred_val))
+print(accuracy_List)
+
+
+# Create a figure of size 8x6 inches, 80 dots per inch
+plt.figure(figsize=(8, 6), dpi=80)
+bx=axes([0.20,0.20,.96-0.20,0.86-0.18])
+plt.grid(linestyle='--', linewidth=0.5)
+plt.plot(list_K, accuracy_List,color="blue",linewidth=1.0,linestyle="-")
+plt.title('Accuracy sur validation test pour $K \in [1;10]$')
+bx.set_xlabel(r'$K$')
+bx.set_ylabel(r'Accuracy')
+
+plt.show()
